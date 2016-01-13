aa8c9e5d43ac0703d1d70b511561c95811c5841a
Wren Withers <wrenwithers@Wrens-MacBook-Air.local>
2016-01-13 14:34:57 -0800

    Make Fear Work

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
   var fear = prompt("What scares you?");
 
-  if (fear === spiders) {
-    $('#spiders').click();
-  } else if (fear === snakes) {
-    $('#snakes').click();
-  } else (fear === sharks)
-    $('#sharks').click();
+  if (fear === "spiders") {
+    location.href =('spiders.html');
+  } else if (fear === "snakes") {
+    location.href =('snakes.html');
+  } else if (fear === "sharks")
+    location.href =('sharks.html');
   });
